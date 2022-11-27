fedecc3c28faab3a286224b66a9de63cf86f9c1e
Andy <alipinski@mail.com>
2022-11-27 19:02:16 +0100

    setting up server with routes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+////////////////////////////////
+//FILES
 // // blocking, syncrhonous
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
@@ -9,16 +13,39 @@ const fs = require('fs');
 // fs.writeFileSync('./txt/output.txt', textOut);
 // // console.log(textOut);
 
-// non-blocking, async
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+///////////////////////////////////////
+// // non-blocking, async
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('error', err);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('file have been written');
-      });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('file have been written');
+//       });
+//     });
+//   });
+// });
+
+////////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is an overview');
+  } else if (pathName === '/product') {
+    res.end('This is the product');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
     });
-  });
+    res.end('<h1>404 page not found</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('server has been started on port 8000');
 });
